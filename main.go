9122f3d37a95580d804133e2902568c625dd9f57
aibin <lab366@163.com>
2024-10-17 22:29:35 +0800

    db

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 	"net/http"
 )
 
@@ -21,56 +19,56 @@ func main() {
 	})
 
 	// 使用dsn格式定义数据库连接信息
-	dsn := "root:123456@tcp(localhost:3306)/yueji_test"
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-
-		}
-	}(db)
-
-	// 测试连接
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// 执行查询
-	rows, err := db.Query("SELECT * FROM dict_city LIMIT 10")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(rows *sql.Rows) {
-		err := rows.Close()
-		if err != nil {
-
-		}
-	}(rows)
-
-	// 处理查询结果
-	for rows.Next() {
-		// ... 假设你的表有一些列，你需要定义相应的变量来接收
-		// 例如 var id int, name string
-		var id int64
-		var cityCn string
-		err := rows.Scan(&id, &cityCn)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(id, cityCn)
-	}
-
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Successfully connected and queried the database!")
-	err = http.ListenAndServe(":8080", nil)
+	//dsn := "root:123456@tcp(localhost:3306)/yueji_test"
+	//db, err := sql.Open("mysql", dsn)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer func(db *sql.DB) {
+	//	err := db.Close()
+	//	if err != nil {
+	//
+	//	}
+	//}(db)
+	//
+	//// 测试连接
+	//err = db.Ping()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//// 执行查询
+	//rows, err := db.Query("SELECT * FROM dict_city LIMIT 10")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer func(rows *sql.Rows) {
+	//	err := rows.Close()
+	//	if err != nil {
+	//
+	//	}
+	//}(rows)
+	//
+	//// 处理查询结果
+	//for rows.Next() {
+	//	// ... 假设你的表有一些列，你需要定义相应的变量来接收
+	//	// 例如 var id int, name string
+	//	var id int64
+	//	var cityCn string
+	//	err := rows.Scan(&id, &cityCn)
+	//	if err != nil {
+	//		fmt.Println(err)
+	//		return
+	//	}
+	//	fmt.Println(id, cityCn)
+	//}
+	//
+	//if err = rows.Err(); err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println("Successfully connected and queried the database!")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
 	}
