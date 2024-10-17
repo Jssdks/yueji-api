bf8a4469abcdf61a672e4cb259f1a0c09dc4847b
aibin <lab366@163.com>
2024-10-17 19:16:17 +0800

    api

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
  
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello, World!")
+        fmt.Fprintln(w, "YUEJI API")
     })
  
     http.ListenAndServe(":8080", nil)
