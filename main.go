b97523edbb3976ca389426261675a08493b2f079
aibin <lab366@163.com>
2024-10-18 10:38:57 +0800

    db

--- main.go
+++ main.go
@@ -13,13 +13,16 @@ import (
 
 var ctx = context.Background()
 
+func p(w http.ResponseWriter, s ...interface{}) {
+	_, err := fmt.Fprintln(w, s)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintln(w, "YUEJI API!")
-		if err != nil {
-			return
-		}
-		// 使用dsn格式定义数据库连接信息
+		p(w, "YUEJI API!")
 		dsn := "root:123456@tcp(localhost:3306)/yueji_test"
 		db, err := sql.Open("mysql", dsn)
 		if err != nil {
@@ -31,13 +34,11 @@ func main() {
 				panic(err)
 			}
 		}(db)
-
-		// 测试连接
 		err = db.Ping()
 		if err != nil {
 			panic(err)
 		}
-
+		p(w, "DB连接成功")
 		// 执行查询
 		rows, err := db.Query("SELECT id,city_cn FROM dict_city LIMIT 10")
 		if err != nil {
@@ -49,48 +50,36 @@ func main() {
 				panic(err)
 			}
 		}(rows)
-		_, err = fmt.Fprintln(w, "DB 连接成功")
-		if err != nil {
-			return
-		}
-		// 处理查询结果
 		for rows.Next() {
-			// ... 假设你的表有一些列，你需要定义相应的变量来接收
-			// 例如 var id int, name string
 			var id int64
 			var cityCn string
 			err := rows.Scan(&id, &cityCn)
 			if err != nil {
 				panic(err)
 			}
-			_, err = fmt.Fprintln(w, id, cityCn)
-			if err != nil {
-				return
-			}
+			p(w, &id, &cityCn)
 		}
 
 		if err = rows.Err(); err != nil {
 			log.Fatal(err)
 		}
-		_, err = fmt.Fprintln(w, "DB读取成功")
-		if err != nil {
-			return
-		}
+		p(w, "DB读取成功")
 		rdb := redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",   // Redis地址
-			Password: "k7z9x*t[j=M^5){e", // Redis密码，如果没有则为空字符串
-			DB:       7,                  // 使用默认DB
+			Addr:     "localhost:6379",
+			Password: "k7z9x*t[j=M^5){e",
+			DB:       7,
 		})
-		_, err = fmt.Fprintln(w, "REDIS连接成功")
-		if err != nil {
-			return
-		}
-		// 设置键值
+		defer func(rdb *redis.Client) {
+			err = rdb.Close()
+			if err != nil {
+				panic(err)
+			}
+		}(rdb)
+		p(w, "REDIS连接成功")
 		err = rdb.Set(ctx, "go-key", time.Now().String(), 0).Err()
 		if err != nil {
 			panic(err)
 		}
-		// 获取键值
 		val, err := rdb.Get(ctx, "go-key").Result()
 		if err != nil {
 			panic(err)
@@ -99,21 +88,7 @@ func main() {
 		if err != nil {
 			return
 		}
-
-		//// 删除键
-		//err = rdb.Del(ctx, "key").Err()
-		//if err != nil {
-		//	panic(err)
-		//}
-		_, err = fmt.Fprintln(w, "DB读取成功")
-		if err != nil {
-			return
-		}
-		// 关闭连接
-		err = rdb.Close()
-		if err != nil {
-			panic(err)
-		}
+		p(w, "REDIS读取成功")
 	})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
